c8f09407040a02457d5196ef11fca26282fece46
Brice Dereims <bdereims@gmail.com>
2021-03-27 10:55:49 +0100

    initial

---
+++ install/grease-monkey/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#bdereims@gmail.com | cloud-garage project
+#configure after creation of grease-monkey template
+#launch under root account
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt update && apt -y upgrade
+apt install -y vim 
+
+mkdir ~/.ssh
+cp authorized_keys ~/.ssh/.
+cp sshd_config /etc/ssh/.
+systemctl restart sshd
+
+cp bashrc ~/.bashrc
+
+cp extend-rootfs.sh generate-machine-id.sh ~/.
+
+cp sysctl.conf /etc/.
+sysctl -p
+
+./generate-motd.sh > /etc/motd
+cp /dev/null /etc/issue
+cp /dev/null /etc/issue.net
