45ca8d713ff9b3fe3c1f5c43ef8ea9f20f56403e
Brice Dereims <bdereims@gmail.com>
2021-03-27 11:24:54 +0100

    initial
    	modified:   install/cgFence/bootstrap.sh
    	new file:   install/cgFence/generate-motd.sh

--- install/cgFence/bootstrap.sh
+++ install/cgFence/bootstrap.sh
@@ -2,8 +2,12 @@
 #bdereims@gmail.com | cloud-garage project
 #configure cgFence after cloning grease-monkey template
 
+sed -i 's/grease-monkey/cgFence/g' /etc/hosts
+sed -i 's/grease-monkey/cgFence/g' /etc/hostname
+
 apt update && apt -y upgrade
-apt install -y curl jq dnsmasq bird ntp
+apt install -y curl jq dnsmasq bird ntp wireguard
 
-systemctl disable bird6
+systemctl disable bird6 ; systemctl stop bird6
 
+./generate-motd.sh > /etc/motd
---
+++ install/cgFence/generate-motd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#bdereims@gmail.com | cloud-garage project
+#generate /etc/motd in color scheme
+
+echo "$(tput setaf 1)
+
+               ____|                       $(tput setaf 1)
+   __|   _\` |  |    _ \\  __ \\    __|   _ \\ $(tput setaf 2)
+  (     (   |  __|  __/  |   |  (      __/ $(tput setaf 6) 
+ \\___| \\__, | _|  \\___| _|  _| \\___| \\___| $(tput setaf 6) 
+       |___/                             $(tput setaf 6) 
+
+$(tput sgr0)
+"
