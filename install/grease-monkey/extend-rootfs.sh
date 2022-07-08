10f8a3c2346d5bcf75b0a03550963e0500bf5c4a
Brice Dereims <bdereims@gmail.com>
2022-07-08 09:30:53 +0000

    improve extend-rootfs

--- install/grease-monkey/extend-rootfs.sh
+++ install/grease-monkey/extend-rootfs.sh
@@ -3,7 +3,10 @@
 #extemd root fs in order to more room
 #$1 : the new device, ex: /dev/sdb
 
+VG=$(vgs --noheadings | awk '{print $1}')
+LV=$(lvdisplay -C -o "lv_path" --noheadings | sed "s/ //g")
+
 pvcreate ${1}
-vgextend grease-monkey-vg ${1}
-lvextend -l +100%FREE /dev/grease-monkey-vg/root
-resize2fs /dev/grease-monkey-vg/root
+vgextend ${VG} ${1}
+lvextend -l +100%FREE ${LV}
+resize2fs ${LV}
