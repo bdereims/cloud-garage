11e9ed8458abcd09d93e9f58fac26cd8aac7a9c2
Brice Dereims <bdereims@gmail.com>
2022-07-11 09:32:33 +0000

    -

--- install/sixty9/bootstrap.sh
+++ install/sixty9/bootstrap.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 #bdereims@gmail.com | cloud-garage project
-#configure just after creation of sixty9 template, validated for debian stable
+#configure just after creation of sixty9 template, validated for ubuntu lts
 #launch with root account
+
+#set -eux
+#set -o pipefail
+
+COMPOSE_VERSION=1.29.0
+HELM_VERSION=3.5.3
+KUBECTL_VERSION=1.20.0
+SHIP_VERSION=0.40.0
+STERN_VERSION=1.11.0
+GOVC_VERSION=0.23.0
+COMPLETIONS=/etc/bash_completion.d
+VMUSER=sixty9
 
 echo "=== Bootstaping sixty9 jumpox"
 echo ""
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit 1
 fi
 
+lsb_release -d | grep Ubuntu
+
 apt update && apt -y upgrade
-apt install -y vim sudo ntp bash-completion sudo
+apt install -y vim sudo ntp bash-completion sudo jq curl sshpass unzip bash-completion tmux
 
 #optional
 #apt install -y open-vm-tools
 
+chmod -x /etc/update-motd.d/*
+
 mkdir ~/.ssh
 cp authorized_keys ~/.ssh/.
+mkdir -p /home/${VMUSER}/.ssh
+cp authorized_keys /home/${VMUSER}/.ssh/.
+chown -R ${VMUSER}:${VMUSER} /home/${VMUSER}/.ssh
 cp sshd_config /etc/ssh/.
 systemctl restart sshd
 
@@ -45,3 +64,44 @@ cp startup.sh ~/.
 cp startup.service /etc/systemd/system/.
 systemctl daemon-reload
 systemctl enable startup.service
+
+curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-Linux-x86_64 && chmod +x /usr/local/bin/docker-compose
+
+#curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl && chmod +x /usr/local/bin/kubectl
+curl -L -o /usr/local/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x /usr/local/bin/kubectl
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install kubectl /usr/bin
+
+curl -L -o /usr/local/bin/stern https://github.com/wercker/stern/releases/download/${STERN_VERSION}/stern_linux_amd64 && chmod +x /usr/local/bin/stern
+
+curl -L https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz | tar zx -C /usr/local/bin --strip-components=1 linux-amd64/helm
+
+curl -L https://github.com/replicatedhq/ship/releases/download/v${SHIP_VERSION}/ship_${SHIP_VERSION}_linux_amd64.tar.gz | tar zx -C /usr/local/bin ship
+
+curl -L https://github.com/static-linux/static-binaries-i386/raw/4266c69990ae11315bad7b928f85b6c8e605ef14/httping-2.4.tar.gz | tar zx -C /usr/local/bin --strip-components=1 httping-2.4/httping
+
+curl -L  https://github.com/vmware/govmomi/releases/download/v${GOVC_VERSION}/govc_linux_amd64.gz | gunzip > /usr/local/bin/govc
+chmod +x /usr/local/bin/govc
+
+git clone https://github.com/ahmetb/kubectx \
+&& cd kubectx \
+&& mv kubectx /usr/local/bin/kctx \
+&& mv kubens /usr/local/bin/kns \
+&& mv completion/*.bash $COMPLETIONS \
+&& cd .. \
+&& rm -rf kubectx
+
+git clone https://github.com/jonmosco/kube-ps1 \
+&& cp kube-ps1/kube-ps1.sh /etc/profile.d/ \
+&& rm -rf kube-ps1
+
+stern --completion bash > $COMPLETIONS/stern.bash
+kubectl completion bash > $COMPLETIONS/kubectl.bash
+helm completion bash > $COMPLETIONS/helm.bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
+adduser ${VMUSER} docker
+cp daemon.json /etc/docker/.
+systemctl restart docker
